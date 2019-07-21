9b087cf1d9dbf04e4673f875dd95603f4f6f5423
scizorman <tueda1207@gmail.com>
2019-07-22 04:39:29 +0900

    [wip] Add client for TCP netword connections
    This is the implementation of the Client interface.
    Ping and PingContext is not implemented yet.

--- client.go
+++ client.go
@@ -2,6 +2,7 @@ package scpi
 
 import (
 	"context"
+	"net"
 )
 
 // Client is a client of a device controlled using SCPI commands.
@@ -28,4 +29,58 @@ type Client interface {
 
 	// QueryContext queries the device for the results of the specified command.
 	QueryContext(ctx context.Context, cmd string) (res []byte, err error)
+}
+
+// TCPClient is an implementation of the Client interface for TCP network connections.
+type TCPClient struct {
+	conn *net.TCPConn
+}
+
+// Close implements the Client Close method.
+func (c *TCPClient) Close() error {
+	return c.conn.Close()
+}
+
+// Exec implements the Client Exec method.
+func (c *TCPClient) Exec(cmd string) error {
+	return c.ExecContext(context.Background(), cmd)
+}
+
+// ExecContext implements the Client ExecContext method.
+func (c *TCPClient) ExecContext(ctx context.Context, cmd string) error {
+	b := []byte(cmd + "\n")
+	if _, err := c.conn.Write(b); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Ping implements the Client Ping method.
+func (c *TCPClient) Ping() error {
+	return c.PingContext(context.Background())
+}
+
+// PingContext implements the Client PingContext method.
+// TODO: Implement
+func (c *TCPClient) PingContext(ctx context.Context) error {
+	return nil
+}
+
+// Query implements the Client Query method.
+func (c *TCPClient) Query(cmd string) (res []byte, err error) {
+	return c.QueryContext(context.Background(), cmd)
+}
+
+// QueryContext implements the Client QueryContext method.
+func (c *TCPClient) QueryContext(ctx context.Context, cmd string) (res []byte, err error) {
+	if err := c.ExecContext(ctx, cmd); err != nil {
+		return nil, err
+	}
+
+	res = make([]byte, 1024)
+	l, err := c.conn.Read(res)
+	if err != nil {
+		return nil, err
+	}
+	return res[:l], nil
 }
