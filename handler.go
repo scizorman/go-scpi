f8912e7ffd7f46265e274dcd983804677d02f9c6
scizorman <tueda1207@gmail.com>
2019-08-01 17:24:48 +0900

    [change] Use context

--- handler.go
+++ handler.go
@@ -26,30 +26,30 @@ func (h *Handler) Reset() error {
 }
 
 // WaitForComplete waits for all queued operations to complete up to the specified timeout.
-func (h *Handler) WaitForComplete(timeout time.Duration) error {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+func (h *Handler) WaitForComplete(ctx context.Context, timeout time.Duration) error {
+	subCtx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
 	ch := make(chan error, 1)
 	go func() {
-		_, err := h.QueryContext(ctx, "*WAI;*OPC?")
+		_, err := h.QueryContext(subCtx, "*WAI;*OPC?")
 		ch <- err
 	}()
 
 	select {
 	case err := <-ch:
 		return err
-	case <-ctx.Done():
+	case <-subCtx.Done():
 		// TODO(scizorman): Refactor the error
-		return ctx.Err()
+		return subCtx.Err()
 	}
 }
 
 // Trigger triggers the device if, and only if,
 // Bus Triggering is the type of trigger event selected.
 // Otherwise, this command is ignored.
-func (h *Handler) Trigger() error {
-	return h.Exec("*TRG")
+func (h *Handler) Trigger(ctx context.Context) error {
+	return h.ExecContext(ctx, "*TRG")
 }
 
 // Identify returns the identification data.
@@ -59,8 +59,8 @@ func (h *Handler) Trigger() error {
 //     Model number
 //     Serial number (or 0)
 //     Firmware version
-func (h *Handler) Identify() (id string, err error) {
-	res, err := h.Query("*IDN?")
+func (h *Handler) Identify(ctx context.Context) (id string, err error) {
+	res, err := h.QueryContext(ctx, "*IDN?")
 	if err != nil {
 		return "", nil
 	}
@@ -71,14 +71,14 @@ func (h *Handler) Identify() (id string, err error) {
 
 // SetEventStatusEnable sets the value in the enable register for the Standard Event Status group.
 // The selected bits are then reported to bit 5 of the Status Byte.
-func (h *Handler) SetEventStatusEnable(bit uint8) error {
+func (h *Handler) SetEventStatusEnable(ctx context.Context, bit uint8) error {
 	cmd := fmt.Sprintf("*ESE %d", bit)
 	return h.Exec(cmd)
 }
 
 // QueryEventStatusEnable queries the event status enable.
-func (h *Handler) QueryEventStatusEnable() (bit uint8, err error) {
-	res, err := h.Query("*ESE?")
+func (h *Handler) QueryEventStatusEnable(ctx context.Context) (bit uint8, err error) {
+	res, err := h.QueryContext(ctx, "*ESE?")
 	if err != nil {
 		return 0, err
 	}
@@ -88,8 +88,8 @@ func (h *Handler) QueryEventStatusEnable() (bit uint8, err error) {
 
 // QueryEventStatusRegister queries the event status register.
 // The register is cleared when it is executed.
-func (h *Handler) QueryEventStatusRegister() (bit uint8, err error) {
-	res, err := h.Query("*ESR?")
+func (h *Handler) QueryEventStatusRegister(ctx context.Context) (bit uint8, err error) {
+	res, err := h.QueryContext(ctx, "*ESR?")
 	if err != nil {
 		return 0, err
 	}
@@ -98,14 +98,14 @@ func (h *Handler) QueryEventStatusRegister() (bit uint8, err error) {
 }
 
 // SetServiceRequestEnable sets the value of the Service Request Enable register.
-func (h *Handler) SetServiceRequestEnable(bit uint8) error {
+func (h *Handler) SetServiceRequestEnable(ctx context.Context, bit uint8) error {
 	cmd := fmt.Sprintf("*SRE %d", bit)
-	return h.Exec(cmd)
+	return h.ExecContext(ctx, cmd)
 }
 
 // QueryServiceRequestEnable queries the Service Request Enable.
-func (h *Handler) QueryServiceRequestEnable() (bit uint8, err error) {
-	res, err := h.Query("*SRE?")
+func (h *Handler) QueryServiceRequestEnable(ctx context.Context) (bit uint8, err error) {
+	res, err := h.QueryContext(ctx, "*SRE?")
 	if err != nil {
 		return 0, err
 	}
@@ -114,8 +114,8 @@ func (h *Handler) QueryServiceRequestEnable() (bit uint8, err error) {
 }
 
 // QueryStatusByteRegister queries the Status Byte Register.
-func (h *Handler) QueryStatusByteRegister() (bit uint8, err error) {
-	res, err := h.Query("*STB?")
+func (h *Handler) QueryStatusByteRegister(ctx context.Context) (bit uint8, err error) {
+	res, err := h.QueryContext(ctx, "*STB?")
 	if err != nil {
 		return 0, err
 	}
@@ -125,15 +125,15 @@ func (h *Handler) QueryStatusByteRegister() (bit uint8, err error) {
 
 // Recall restored the instrument to a state that was previously stored
 // in locations 0 through 9 with the Save.
-func (h *Handler) Recall(mem uint8) error {
+func (h *Handler) Recall(ctx context.Context, mem uint8) error {
 	cmd := fmt.Sprintf("*RCL %d", mem)
-	return h.Exec(cmd)
+	return h.ExecContext(ctx, cmd)
 }
 
 // Save saves the instrument setting to one of the ten non-volatile memory locations.
-func (h *Handler) Save(mem uint8) error {
+func (h *Handler) Save(ctx context.Context, mem uint8) error {
 	cmd := fmt.Sprintf("*SAV %d", mem)
-	return h.Exec(cmd)
+	return h.ExecContext(ctx, cmd)
 }
 
 var bitRegexp = regexp.MustCompile(`\+(\d+)`)
